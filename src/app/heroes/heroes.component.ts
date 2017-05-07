dffbe2ece02cf3fb378c03c85be4c4bbfd387a75
t301000 <t301000@gmail.com>
2017-05-07 19:45:04 +0800

    實作 addHero
    實作 addHero
    使用樣板參考變數

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,9 +11,20 @@ import { Hero } from '../hero';
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = HEROES;
   selectedHero: Hero = null;
+  hintWord = 'New Hero\'s name';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addHero(name: string) {
+    name = name.trim();
+    if (! name) {
+      return;
+    }
+    const id = this.heroes[this.heroes.length - 1].id + 1;
+    this.heroes.push({id, name});
   }
 
 }
