c96a5b3fb182555c79f3798c7227e5c286b1003b
林士立 <t301000@gmail.com>
2017-05-08 12:00:55 +0800

    改寫 hero service
    hero service 改為使用 Observable

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,27 +1,52 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http, RequestOptions, Response } from '@angular/http';
 
-import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+// import 'rxjs/add/operator/do';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class HeroService {
-  heroes: Hero[] = HEROES;
-  nextId: number = this.heroes[this.heroes.length - 1].id + 1;
+  baseUrl = '/api/heroes';
 
-  constructor() { }
+  constructor(private http: Http) { }
 
-  getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(this.heroes);
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get(this.baseUrl)
+              .map((res: Response) => res.json().data || {})
+              .catch(this.handleError);
   }
 
-  addHero(name: string): Promise<Hero> {
-    const newHero: Hero = {id: this.nextId++, name};
-    this.heroes = [...this.heroes, newHero];
-    return Promise.resolve(newHero);
+  addHero(name: string): Observable<Hero> {
+    const headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+    const options = new RequestOptions({headers});
+    return this.http.post(this.baseUrl, {name}, options)
+              .map((res: Response) => res.json().data)
+              .catch(this.handleError);
   }
 
-  deleteHero(id: number): Promise<any> {
-    this.heroes = this.heroes.filter(hero => hero.id !== id);
-    return Promise.resolve();
+  deleteHero(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`)
+              .map((res: Response) => null)
+              .catch(this.handleError);
+  }
+
+  private handleError(error: Response | any): Observable<any> {
+    // console.log(error);
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
   }
 }
